5448af70bc7c11a6e7818d49fc643d8ccedba14b
Emori03 <emorijan@gmail.com>
2023-09-27 18:16:05 -0600

    Creacion del boton de borrar

--- Calculadora/index.js
+++ Calculadora/index.js
@@ -6,6 +6,13 @@ function clearDisplay() {
     document.getElementById("display").value = "";
 }
 
+function deleteDisplay() {
+    let Display = document.getElementById("display").value;
+    console.log(Display);
+    document.getElementById("display").value = Display.substring(0, Display.length - 1);
+    console.log(Display);
+}
+
 function calculateResult() {
     const expression = document.getElementById("display").value;
     try {
