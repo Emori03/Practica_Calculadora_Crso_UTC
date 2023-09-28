d608776c25a68885cf6ef616472b5a46eedaf439
Emori03 <emorijan@gmail.com>
2023-09-27 19:39:29 -0600

    Se agregaron funciones para resivir las teclas de los dispositivos

--- Calculadora/index.js
+++ Calculadora/index.js
@@ -20,3 +20,42 @@ function calculateResult() {
         document.getElementById("display").value = "Error";
     }
 }
+
+document.addEventListener('keydown', function(event) {
+    let button
+    if (event.key === 'Enter'){
+        button = document.getElementById('btn=');
+    } else if (event.key === 'Delete'){
+        button = document.getElementById('btnDelete');
+    } else if (event.key === 'Backspace'){
+        button = document.getElementById('btnC');
+    }
+    if (event.key === '1') {
+        button = document.getElementById('btn1');
+    } else if (event.key === '2'){
+        button = document.getElementById('btn2');
+    } else if (event.key === '3'){
+        button = document.getElementById('btn3');
+    } else if (event.key === '4'){
+        button = document.getElementById('btn4');
+    } else if (event.key === '5'){
+        button = document.getElementById('btn5');
+    } else if (event.key === '6'){
+        button = document.getElementById('btn6');
+    } else if (event.key === '7'){
+        button = document.getElementById('btn7');
+    } else if (event.key === '8'){
+        button = document.getElementById('btn8');
+    } else if (event.key === '9'){
+        button = document.getElementById('btn9');
+    } else if (event.key === '+'){
+        button = document.getElementById('btn+');
+    } else if (event.key === '-'){
+        button = document.getElementById('btn-');
+    } else if (event.key === '/'){
+        button = document.getElementById('btn/');
+    } else if (event.key === '*'){
+        button = document.getElementById('btn*');
+    }
+    button.click();
+  });
